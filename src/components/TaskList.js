c44c2ee68b401e9053f33864d707fe6e5a4df66d
gokce <gokcetascii@gmail.com>
2024-06-19 12:29:39 +0300

    Created a search entry for the task list, and added the number of tasks to be displayed according to the screen height, added editing style and responsive design.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { MdDeleteForever, MdEdit } from "react-icons/md";
+import { MdDeleteForever, MdEdit, MdSearch } from "react-icons/md";
 import { HiDotsVertical } from "react-icons/hi";
 import Select from "react-select";
 import { FcDocument } from "react-icons/fc";
@@ -8,9 +8,10 @@ import DetailTask from "./DetailTask";
 const TaskList = ({ tasks, deleteTask, editTask, updateTaskStatus }) => {
   const [currentPage, setCurrentPage] = useState(1); // Mevcut sayfa numarası
   const [totalPages, setTotalPages] = useState(1); // Toplam sayfa sayısı
-  const tasksPerPage = 6; // Sayfa başına gösterilecek task sayısı
+  const [tasksPerPage, setTasksPerPage] = useState(6); // Sayfa başına gösterilecek task sayısı
   const [showDetail, setShowDetail] = useState(false); // Detayın gösterilip gösterilmeyeceğini tutan state
   const [selectedTask, setSelectedTask] = useState(null); // Gösterilen taskin bilgisi
+  const [searchTerm, setSearchTerm] = useState(""); // Arama inputu
 
   // React Select için seçeneklerin listesi
   const statusOptions = [
@@ -80,10 +81,15 @@ const TaskList = ({ tasks, deleteTask, editTask, updateTaskStatus }) => {
     }
   }, [tasks, currentPage, tasksPerPage]); // tasks, currentPage ve tasksPerPage değiştiğinde useEffect'i tetikle
 
-  // Sayfadaki görevleri belirle
   const indexOfLastTask = currentPage * tasksPerPage; // Son taskin index'i
+
   const indexOfFirstTask = indexOfLastTask - tasksPerPage; // İlk taskin index'i
-  const currentTasks = tasks.slice(indexOfFirstTask, indexOfLastTask); // Mevcut sayfadaki taskleri al
+
+  const currentTasks = tasks
+    .filter((task) =>
+      task.title.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    .slice(indexOfFirstTask, indexOfLastTask); // Arama işlemi burada gerçekleşiyor
 
   useEffect(() => {
     // İlk yüklendiğinde mevcut sayfa numarasını 1 olarak ayarla
@@ -102,22 +108,60 @@ const TaskList = ({ tasks, deleteTask, editTask, updateTaskStatus }) => {
     setSelectedTask(null);
   };
 
+  useEffect(() => {
+    // Ekran yüksekliğine bağlı olarak sayfa başına gösterilecek görev sayısını güncelleyen fonksiyon
+    const updateTasksPerPageHeight = () => {
+      const height = window.innerHeight;
+      if (height > 1500) {
+        setTasksPerPage(20); // Ekran yüksekliği 1500 pikselden büyükse 20 görev göster
+      } else if (height > 1200) {
+        setTasksPerPage(16); // Ekran yüksekliği 1200 pikselden büyükse 16 görev göster
+      } else if (height > 800) {
+        setTasksPerPage(9); // Ekran yüksekliği 800 pikselden büyükse 9 görev göster
+      } else {
+        setTasksPerPage(6); // Ekran yüksekliği 800 pikselden küçükse 6 görev göster
+      }
+    };
+  
+    updateTasksPerPageHeight(); // İlk yüklemede fonksiyonu çağır
+    window.addEventListener("resize", updateTasksPerPageHeight); // Ekran boyutu değişikliklerini dinle ve fonksiyonu çağır
+    return () => window.removeEventListener("resize", updateTasksPerPageHeight); // Bileşen temizlendiğinde event listener'ı kaldır
+  }, []);
+  
   return (
-    <div id="tasklist">
-      <div className="h-[450px] ">
-        <table className="px-5 py-3 text-[16px] rounded-md w-[1100px]">
-          <thead className="px-5 py-3 text-gray-800 tracking-wide ">
+    <div id="tasklist" className="flex flex-col w-screen md:w-auto">
+      <div className="flex absolute top-24 md:top-14 text-[10px] lg:text-[16px] ml-6 md:ml-0">
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="p-2 pl-4 mb-4 sm:w-[250px] rounded-lg border-2 border-gray-300 outline-none hover:border-blue-400 transition-all duration-500 ease-in-out transform"
+          />
+          <MdSearch className="absolute top-2 right-2 w-6 h-6 text-blue-400" />
+        </div>
+      </div>
+      <div className="">
+        <table className="px-5 py-3 text-[16px] rounded-md w-full md:w-[600px] lg:w-[800px] xl:w-[1100px] text-[10px] md:text-[12px] xl:text-[16px] overflow-auto md:overflow-scroll">
+          <thead className="md:px-5 py-3 text-gray-800 tracking-wide ">
             <tr className="border-b border-gray-200 font-bold">
-              <th className="px-5 py-3 text-center max-w-[100px]">Title</th>
-              <th className="px-5 py-3 text-center max-w-[100px]">
+              <th className="md:px-5 py-3 text-center xl:max-w-[100px]">
+                Title
+              </th>
+              <th className="md:px-5 py-3 text-center xl:max-w-[100px]">
                 Description
               </th>
-              <th className="px-5 py-3 text-center max-w-[100px]">Category</th>
-              <th className="px-5 py-3 text-center max-w-[100px]">Status</th>
-              <th className="px-5 py-3 text-center max-w-[100px]">
+              <th className="md:px-5 py-3 text-center xl:max-w-[100px]">
+                Category
+              </th>
+              <th className="md:px-5 py-3 text-center lg:max-w-[100px]">
+                Status
+              </th>
+              <th className="md:px-5 py-3 text-center xl:max-w-[100px]">
                 Completion Date
               </th>
-              <th className="px-5 py-3 text-center max-w-[100px]">
+              <th className="md:px-5 py-3 text-center xl:max-w-[100px]">
                 <div className="flex items-center justify-center">
                   <HiDotsVertical className="w-5 h-5" />
                 </div>
@@ -134,16 +178,16 @@ const TaskList = ({ tasks, deleteTask, editTask, updateTaskStatus }) => {
                     : ""
                 }`}
               >
-                <td className="px-5 py-3 text-center max-w-[100px] text-blue-400 font-bold text-ellipsis overflow-hidden ">
+                <td className="md:px-5 py-3 text-center xl:max-w-[100px] text-blue-400 font-bold text-ellipsis overflow-hidden ">
                   {task.title}
                 </td>
-                <td className="px-5 py-3 text-center max-w-[100px] text-ellipsis overflow-hidden ">
+                <td className="md:px-5 py-3 text-center xl:max-w-[100px] text-ellipsis overflow-hidden ">
                   {task.description}
                 </td>
-                <td className="px-5 py-3 text-center max-w-[100px]">
+                <td className="md:px-5 py-3 text-center xl:max-w-[100px]">
                   {task.category ? task.category : "-"}
                 </td>
-                <td className="px-5 py-3 text-center max-w-[100px]">
+                <td className="md:px-5 py-3 text-center xl:max-w-[100px]">
                   <Select
                     value={
                       statusOptions.find(
@@ -155,25 +199,25 @@ const TaskList = ({ tasks, deleteTask, editTask, updateTaskStatus }) => {
                     }
                     options={statusOptions}
                     styles={customStyles}
-                    className="w-[150px]"
+                    className="max-w-[150px]"
                   />
                 </td>
-                <td className="px-5 py-3 text-center max-w-[100px]">
+                <td className="md:px-5 py-3 text-center w-[40px] xl:max-w-[100px]">
                   {task.status === "Completed"
                     ? new Date(task.completionDate).toLocaleDateString()
                     : "-"}
                 </td>
-                <td className="px-5 py-3 text-center max-w-[100px]">
+                <td className="md:px-5 py-3 text-center xl:max-w-[100px]">
                   <div className="flex flex-row gap-2 items-center justify-center">
                     <button onClick={() => openDetail(task)}>
-                      <FcDocument className="fill-blue-500 w-6 h-6 hover:scale-110 transition-all duration-500 ease-in-out transform" />
+                      <FcDocument className="fill-blue-500 lg:w-6 lg:h-6 hover:scale-110 transition-all duration-500 ease-in-out transform" />
                     </button>
 
                     <button onClick={() => editTask(task)}>
-                      <MdEdit className="fill-blue-500 w-6 h-6 hover:scale-110 transition-all duration-500 ease-in-out transform" />
+                      <MdEdit className="fill-blue-500 lg:w-6 lg:h-6 hover:scale-110 transition-all duration-500 ease-in-out transform" />
                     </button>
                     <button onClick={() => deleteTask(task)}>
-                      <MdDeleteForever className="fill-red-500 w-6 h-6 hover:scale-110 transition-all duration-500 ease-in-out transform" />
+                      <MdDeleteForever className="fill-red-500 lg:w-6 lg:h-6 hover:scale-110 transition-all duration-500 ease-in-out transform" />
                     </button>
                   </div>
                 </td>
@@ -186,15 +230,15 @@ const TaskList = ({ tasks, deleteTask, editTask, updateTaskStatus }) => {
       {/* Sayfa Numaraları */}
       <div className="flex justify-end mt-4">
         <nav>
-          <ul className="flex list-none">
+          <ul className="flex list-none text-[10px] xl:text-[16px]">
             {[...Array(totalPages)].map((_, index) => (
               <li key={index} className="mx-1">
                 <button
                   onClick={() => paginate(index + 1)}
-                  className={`px-3 py-1 rounded-full ${
+                  className={`px-2 lg:px-3 py-1 rounded-full ${
                     currentPage === index + 1
-                      ? "bg-blue-500 text-white"
-                      : "bg-gray-200 text-gray-800"
+                      ? "bg-blue-400 text-white hover:scale-105 transition-all duration-500 ease-in-out transform "
+                      : "bg-gray-200 text-gray-800 hover:scale-105 transition-all duration-500 ease-in-out transform hover:bg-blue-200 "
                   }`}
                 >
                   {index + 1}
